505b212e5ad0486958246603779760344b58db15
Hugo Branco <hugobrancowb@gmail.com>
2022-06-29 16:45:07 -0300

    refactor: :hammer: :racehorse: Refactor to process images using parallel computing (isolates)
    Signed-off-by: Hugo Branco <hugobrancowb@gmail.com>

--- lib/widgets/bill_scan_camera.widget.dart
+++ lib/widgets/bill_scan_camera.widget.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:barcode_bill_scanner/util/process_image.isolate.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_barcode_kit/google_barcode_kit.dart';
+import 'package:rxdart/rxdart.dart';
 
 /// Widget used to read the barcode using phone's camera.
 class BillScanCameraWidget extends StatefulWidget {
@@ -12,114 +16,135 @@ class BillScanCameraWidget extends StatefulWidget {
     this.initialDirection = CameraLensDirection.back,
   }) : super(key: key);
 
-  final Function(InputImage inputImage) onImage;
+  final Function(List<Barcode> barcodes) onImage;
   final CameraLensDirection initialDirection;
 
   @override
-  _BillScanCameraWidgetState createState() => _BillScanCameraWidgetState();
+  BillScanCameraWidgetState createState() => BillScanCameraWidgetState();
 }
 
-class _BillScanCameraWidgetState extends State<BillScanCameraWidget> {
+class BillScanCameraWidgetState extends State<BillScanCameraWidget> {
+  /// Camera list.
   List<CameraDescription> cameras = [];
-  CameraController? _controller;
-  int _cameraIndex = 0;
-  double zoomLevel = 0.0, minZoomLevel = 0.0, maxZoomLevel = 0.0;
+
+  /// Controller for the camera.
+  CameraController? cameraController;
+
+  /// Stream of images to be processed.
+  final StreamController<CameraImage> streamController = StreamController<CameraImage>();
+
+  /// Subscription of images being processed.
+  late final StreamSubscription streamSubscription;
+
+  /// Port used to receive processed images.
+  final ReceivePort receivePort = ReceivePort('imageProcessPort');
+
+  /// Camera being used for scanning.
+  int? _cameraIndex;
+
+  /// Zoom levels.
+  double zoomLevel = 1.0, minZoomLevel = 0.0;
+
+  /// Scanner for reading barcode images.
+  final BarcodeScanner barcodeScanner = GoogleMlKit.vision.barcodeScanner([
+    BarcodeFormat.itf,
+    BarcodeFormat.codabar,
+  ]);
+
+  @override
+  void initState() {
+    super.initState();
+    initStreams();
+  }
 
   @override
   void didChangeDependencies() async {
     super.didChangeDependencies();
     cameras = await availableCameras();
     for (var i = 0; i < cameras.length; i++) {
-      if (cameras[i].lensDirection == widget.initialDirection) {
+      if (cameras[i].lensDirection == widget.initialDirection && _cameraIndex == null) {
         _cameraIndex = i;
       }
     }
-    _startLiveFeed();
+
+    startLiveFeed();
   }
 
   @override
   void dispose() {
-    _stopLiveFeed();
+    stopLiveFeed();
     super.dispose();
+    streamSubscription.cancel();
+    streamController.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    return _controller?.value.isInitialized == true
+    return cameraController?.value.isInitialized == true
         ? Container(
             height: double.infinity,
             width: double.infinity,
             color: Colors.black,
-            child: Center(child: CameraPreview(_controller!)),
+            child: Center(child: CameraPreview(cameraController!)),
           )
         : Container();
   }
 
-  Future _startLiveFeed() async {
-    final camera = cameras[_cameraIndex];
-    _controller = CameraController(
+  /// Initialize event streams used to process camera images.
+  void initStreams() {
+    // buffers livestream to process only one image per second.
+    // sends a message to parallel processing.
+    streamSubscription = streamController.stream
+        .bufferTime(const Duration(seconds: 1))
+        .where((imageList) => imageList.isNotEmpty)
+        .map((imageList) => imageList.last)
+        .listen(convertsCameraImage);
+
+    // translates incoming images to barcode.
+    // receives a message from parallel processing.
+    receivePort
+        .asBroadcastStream()
+        .whereType<InputImage>()
+        .asyncMap((InputImage inputImage) => barcodeScanner.processImage(inputImage))
+        .where((List<Barcode> barcodeList) => barcodeList.isNotEmpty)
+        .listen(widget.onImage);
+  }
+
+  /// Begins camera livestream.
+  Future<void> startLiveFeed() async {
+    final camera = cameras[_cameraIndex ?? 0];
+    cameraController = CameraController(
       camera,
       ResolutionPreset.high,
       enableAudio: false,
     );
-    _controller?.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      _controller?.getMinZoomLevel().then((value) {
-        zoomLevel = value;
-        minZoomLevel = value;
-      });
-      _controller?.getMaxZoomLevel().then((value) {
-        maxZoomLevel = value;
-      });
-      _controller?.lockCaptureOrientation(DeviceOrientation.portraitUp);
-      _controller?.startImageStream(_processCameraImage);
-      setState(() {});
+
+    await cameraController?.initialize();
+
+    if (!mounted) return;
+
+    cameraController?.getMinZoomLevel().then((value) {
+      zoomLevel = value;
+      minZoomLevel = value;
     });
+
+    cameraController?.setZoomLevel(zoomLevel > minZoomLevel ? zoomLevel : minZoomLevel);
+
+    cameraController?.lockCaptureOrientation(DeviceOrientation.portraitUp);
+    cameraController?.startImageStream(streamController.add);
+    setState(() {});
   }
 
-  Future _stopLiveFeed() async {
-    await _controller?.stopImageStream();
-    await _controller?.dispose();
-    _controller = null;
+  /// Stops camera livestream.
+  Future stopLiveFeed() async {
+    await cameraController?.stopImageStream();
+    await cameraController?.dispose();
   }
 
-  Future _processCameraImage(CameraImage image) async {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (Plane plane in image.planes) {
-      allBytes.putUint8List(plane.bytes);
-    }
-    final bytes = allBytes.done().buffer.asUint8List();
-
-    final Size imageSize = Size(image.width.toDouble(), image.height.toDouble());
-
-    final camera = cameras[_cameraIndex];
-    final imageRotation = InputImageRotationMethods.fromRawValue(camera.sensorOrientation) ??
-        InputImageRotation.Rotation_0deg;
-
-    final inputImageFormat =
-        InputImageFormatMethods.fromRawValue(image.format.raw) ?? InputImageFormat.NV21;
-
-    final planeData = image.planes.map(
-      (Plane plane) {
-        return InputImagePlaneMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          height: plane.height,
-          width: plane.width,
-        );
-      },
-    ).toList();
-
-    final inputImageData = InputImageData(
-      size: imageSize,
-      imageRotation: imageRotation,
-      inputImageFormat: inputImageFormat,
-      planeData: planeData,
-    );
-
-    final inputImage = InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
-
-    widget.onImage(inputImage);
+  /// Sends captured image to be converted from [CameraImage] type into [InputImage] type.
+  ///
+  /// The converted imaged is received by [listenBarcodes] method.
+  Future<void> convertsCameraImage(CameraImage image) async {
+    processCameraImage(receivePort, image);
   }
 }
